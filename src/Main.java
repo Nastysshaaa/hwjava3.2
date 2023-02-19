0f7a40349948581ce890f51af1dfb4c8c27470c3
Anastasiia <anastasia994@yandex.ru>
2023-02-19 17:37:59 +0500

    Change file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        BmiService service = new BmiService();
+        //double height = 1.96;
+        //double weight = 91;
+        float index = service.calculate(91, (float) 1.96);
+        System.out.println(index);
+
+        }
 }
